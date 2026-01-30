2945205ac47a1acc3924aff6c43580be2982dfa6
Nadia <n.tchuemo@gmail.com>
2026-01-30 17:50:04 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "fte5f4o35f8ba2b321d1c0ebb3adf4a1";
   let prompt = `Generate a poem about ${userPrompt.value}`;
   let context =
-    "You are an AI poem generator that creates short, original, and expressive poems based on user input. Adapt tone, style, and structure to match the prompt, using vivid imagery and natural rhythm. Ensure each poem feels unique, emotionally engaging, and human-like. Your mission is to generate a 8 line poem and a heading using basic HTML structure. Don't show HTML and separate each line.";
+    "You are an AI poem generator that creates short, original, and expressive poems based on user input. Adapt tone, style, and structure to match the prompt, using vivid imagery and natural rhythm. Ensure each poem feels unique, emotionally engaging, and human-like. our mission is to generate a 6 line poem and a heading using basic HTML structure and separate in each line. Don't show HTML.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem..");
